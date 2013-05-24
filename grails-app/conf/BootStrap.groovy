6158c58c98b296ab9573be205a7d7c75fbb64f37
Alexandru Bolboaca <alexboly@gmail.com>
2013-05-24 21:10:20 +0300

    Changed default controller to list of attendees

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import keepintouch.*
+
 class BootStrap {
 
     def init = { servletContext ->
+	if(!Attendee.count()){
+		new Attendee(name: "Alexandru Bolboaca").save(failOnError:true)
+	}
     }
     def destroy = {
     }
