610fc55b778c4f14d8c05bfff4b2cdba946be39d
Alexandru Bolboaca <alexboly@gmail.com>
2013-05-28 19:35:04 +0300

    Added empty integration test

---
+++ test/integration/keepintouch/AttendeeTests.groovy
@@ -0,0 +1,22 @@
+package keepintouch
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class AttendeeTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+        assertTrue true
+    }
+}
