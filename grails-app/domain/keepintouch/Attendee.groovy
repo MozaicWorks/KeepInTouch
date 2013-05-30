17f562fc08f5191ede6ae57ad2a63f8b289f866a
aimee rivers <aimee@aimeerivers.com>
2013-05-30 13:26:10 +0100

    Age is mandatory for unit test.

--- grails-app/domain/keepintouch/Attendee.groovy
+++ grails-app/domain/keepintouch/Attendee.groovy
@@ -3,19 +3,19 @@ package keepintouch
 import com.sun.istack.internal.Nullable;
 
 class Attendee {
-    String name
+  String name
     String email
     String location
-	Integer age
-	String twitter
-	String blog
+    Integer age
+    String twitter
+    String blog
 
     static constraints = {
-        name()
-		email( blank: true, nullable : true)
+      name()
+        email( blank: true, nullable : true)
         location( blank : true, nullable : true)
-    	age(range:15..90)
-		twitter(blank: true, nullable : true)
-		blog(blank: true, nullable : true)
-	}
+        age(range:15..90)
+        twitter(blank: true, nullable : true)
+        blog(blank: true, nullable : true)
+    }
 }
--- test/unit/keepintouch/AttendeeTests.groovy
+++ test/unit/keepintouch/AttendeeTests.groovy
@@ -10,11 +10,10 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Attendee)
-class AttendeeTests {
+  class AttendeeTests {
 
     void testSomething() {
-       Attendee attendee = new Attendee(name:"Alex")
-	   
-	   assertEquals("Alex", attendee.name)
+      Attendee attendee = new Attendee(name:"Alex", age: 20)
+      assertEquals("Alex", attendee.name)
     }
-}
+  }
