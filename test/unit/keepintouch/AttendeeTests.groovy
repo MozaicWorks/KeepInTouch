4fda3212d4f8368031a4996c657d1ef645b56d3d
aimee rivers <aimee@aimeerivers.com>
2013-05-30 16:11:20 +0100

    Tidy up indentation in the test file.

--- test/unit/keepintouch/AttendeeTests.groovy
+++ test/unit/keepintouch/AttendeeTests.groovy
@@ -10,26 +10,26 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Attendee)
-  class AttendeeTests {
+class AttendeeTests {
 
-    void testValidAttendee() {
-      Attendee attendee = new Attendee(name:"Alex", age: 20)
-      assertEquals("Alex", attendee.name)
-    }
-
-    void testNameIsMandatory() {
-      Attendee attendee = new Attendee(age: 20)
-      assertFalse attendee.validate()
-    }
-
-    void testAgeTooYoung() {
-      Attendee attendee = new Attendee(name:"Alex", age: 14)
-      assertFalse attendee.validate()
-    }
-
-    void testAgeTooOld() {
-      Attendee attendee = new Attendee(name:"Alex", age: 91)
-      assertFalse attendee.validate()
-    }
-
+  void testValidAttendee() {
+    Attendee attendee = new Attendee(name:"Alex", age: 20)
+    assertEquals("Alex", attendee.name)
   }
+
+  void testNameIsMandatory() {
+    Attendee attendee = new Attendee(age: 20)
+    assertFalse attendee.validate()
+  }
+
+  void testAgeTooYoung() {
+    Attendee attendee = new Attendee(name:"Alex", age: 14)
+    assertFalse attendee.validate()
+  }
+
+  void testAgeTooOld() {
+    Attendee attendee = new Attendee(name:"Alex", age: 91)
+    assertFalse attendee.validate()
+  }
+
+}
