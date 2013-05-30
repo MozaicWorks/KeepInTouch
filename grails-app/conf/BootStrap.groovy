835e810939d03f9c54cf3f72663811967097c10f
Robert Mircea <robertcmircea@gmail.com>
2013-05-30 17:08:34 +0300

    Autocreate upload directory for pictures if it does not exist

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,16 @@ import keepintouch.*
 class BootStrap {
 
     def init = { servletContext ->
+        def storagePath = servletContext.getRealPath("/")
+        def storagePathDirectory = new File("${storagePath}/upload/")
+        if (!storagePathDirectory.exists()) {
+            print "CREATING DIRECTORY ${storagePathDirectory}: "
+            if (storagePathDirectory.mkdirs()) {
+                println "SUCCESS"
+            } else {
+                println "FAILED"
+            }
+        }
     }
     def destroy = {
     }
