2b445961ef98f4283a29483067a0e7255a637377
Vadim Comanescu <vadim984@gmail.com>
2013-05-31 15:11:09 +0300

    Properly handle the picture upload

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,18 +1,15 @@
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+
 import keepintouch.*
 
 class BootStrap {
+	def grailsApplication
 
     def init = { servletContext ->
-        def storagePath = servletContext.getRealPath("/")
-        def storagePathDirectory = new File("${storagePath}/upload/")
-        if (!storagePathDirectory.exists()) {
-            print "CREATING DIRECTORY ${storagePathDirectory}: "
-            if (storagePathDirectory.mkdirs()) {
-                println "SUCCESS"
-            } else {
-                println "FAILED"
-            }
-        }
+		def uploadDir = new File(grailsApplication.config.keepintouch.upload.path)
+		if (!uploadDir.exists()) {
+			println "ERROR: Application file upload directory " + uploadDir + " does not exist ... picture upload will not work"
+		}
     }
     def destroy = {
     }
--- grails-app/controllers/keepintouch/AttendeeController.groovy
+++ grails-app/controllers/keepintouch/AttendeeController.groovy
@@ -5,109 +5,145 @@ import org.springframework.web.multipart.MultipartFile
 
 class AttendeeController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	def grailsApplication
 
-    def index() {
-        redirect(action: "list", params: params)
-    }
+	def index() {
+		redirect(action: "list", params: params)
+	}
 
-    def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        [attendeeInstanceList: Attendee.list(params), attendeeInstanceTotal: Attendee.count()]
-    }
+	def list(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		[attendeeInstanceList: Attendee.list(params), attendeeInstanceTotal: Attendee.count()]
+	}
 
-    def create() {
-        [attendeeInstance: new Attendee(params)]
-    }
+	def create() {
+		[attendeeInstance: new Attendee(params)]
+	}
 
-    def save() {
-        def attendeeInstance = new Attendee(params)
+	def save() {
+		def attendeeInstance = new Attendee(params)
 
-        def file = request.getFile("picture")
-        if(file) {
-            def fileName = file.getOriginalFilename()
-            attendeeInstance.picture = fileName
-            def rootPath = request.getSession().getServletContext().getRealPath("/")
-            file.transferTo(new File("${rootPath}/upload/${fileName}"))
-        }
+		def fileName = null
+		def file = request.getFile("picture")
+		if(file) {
+			fileName = file.getOriginalFilename()
+			attendeeInstance.picture = fileName
+		}
 
-        if (!attendeeInstance.save(flush: true)) {
-            render(view: "create", model: [attendeeInstance: attendeeInstance])
-            return
-        }
+		if (!attendeeInstance.save(flush: true)) {
+			render(view: "create", model: [attendeeInstance: attendeeInstance])
+			return
+		}
+		
+		if(file){
+			file.transferTo(new File("${grailsApplication.config.keepintouch.upload.path}/${fileName}"))
+		}
 
+		flash.message = message(code: 'default.created.message', args: [
+			message(code: 'attendee.label', default: 'Attendee'),
+			attendeeInstance.id
+		])
+		redirect(action: "show", id: attendeeInstance.id)
+	}
 
-        flash.message = message(code: 'default.created.message', args: [message(code: 'attendee.label', default: 'Attendee'), attendeeInstance.id])
-        redirect(action: "show", id: attendeeInstance.id)
-    }
+	def show(Long id) {
+		def attendeeInstance = Attendee.get(id)
+		if (!attendeeInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "list")
+			return
+		}
 
-    def show(Long id) {
-        def attendeeInstance = Attendee.get(id)
-        if (!attendeeInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "list")
-            return
-        }
+		[attendeeInstance: attendeeInstance]
+	}
 
-        [attendeeInstance: attendeeInstance]
-    }
+	def edit(Long id) {
+		def attendeeInstance = Attendee.get(id)
+		if (!attendeeInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "list")
+			return
+		}
 
-    def edit(Long id) {
-        def attendeeInstance = Attendee.get(id)
-        if (!attendeeInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "list")
-            return
-        }
+		[attendeeInstance: attendeeInstance]
+	}
 
-        [attendeeInstance: attendeeInstance]
-    }
+	def update(Long id, Long version) {
+		def attendeeInstance = Attendee.get(id)
+		if (!attendeeInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "list")
+			return
+		}
 
-    def update(Long id, Long version) {
-        def attendeeInstance = Attendee.get(id)
-        if (!attendeeInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "list")
-            return
-        }
+		if (version != null) {
+			if (attendeeInstance.version > version) {
+				attendeeInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[
+							message(code: 'attendee.label', default: 'Attendee')] as Object[],
+						"Another user has updated this Attendee while you were editing")
+				render(view: "edit", model: [attendeeInstance: attendeeInstance])
+				return
+			}
+		}
 
-        if (version != null) {
-            if (attendeeInstance.version > version) {
-                attendeeInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'attendee.label', default: 'Attendee')] as Object[],
-                          "Another user has updated this Attendee while you were editing")
-                render(view: "edit", model: [attendeeInstance: attendeeInstance])
-                return
-            }
-        }
+		attendeeInstance.properties = params
 
-        attendeeInstance.properties = params
+		def file = request.getFile("picture")
+		if(file) {
+			def fileName = file.getOriginalFilename()
+			attendeeInstance.picture = fileName
+		}
+		
+		if (!attendeeInstance.save(flush: true)) {
+			render(view: "edit", model: [attendeeInstance: attendeeInstance])
+			return
+		}
+		
+		file.transferTo(new File("${grailsApplication.config.keepintouch.upload.path}/${fileName}"))
 
-        if (!attendeeInstance.save(flush: true)) {
-            render(view: "edit", model: [attendeeInstance: attendeeInstance])
-            return
-        }
+		flash.message = message(code: 'default.updated.message', args: [
+			message(code: 'attendee.label', default: 'Attendee'),
+			attendeeInstance.id
+		])
+		redirect(action: "show", id: attendeeInstance.id)
+	}
 
-        flash.message = message(code: 'default.updated.message', args: [message(code: 'attendee.label', default: 'Attendee'), attendeeInstance.id])
-        redirect(action: "show", id: attendeeInstance.id)
-    }
+	def delete(Long id) {
+		def attendeeInstance = Attendee.get(id)
+		if (!attendeeInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "list")
+			return
+		}
 
-    def delete(Long id) {
-        def attendeeInstance = Attendee.get(id)
-        if (!attendeeInstance) {
-            flash.message = message(code: 'default.not.found.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "list")
-            return
-        }
-
-        try {
-            attendeeInstance.delete(flush: true)
-            flash.message = message(code: 'default.deleted.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "list")
-        }
-        catch (DataIntegrityViolationException e) {
-            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'attendee.label', default: 'Attendee'), id])
-            redirect(action: "show", id: id)
-        }
-    }
+		try {
+			attendeeInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "list")
+		}
+		catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [
+				message(code: 'attendee.label', default: 'Attendee'),
+				id
+			])
+			redirect(action: "show", id: id)
+		}
+	}
 }
